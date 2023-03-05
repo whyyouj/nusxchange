2fa533fbb6c5f2186d34fe0022d52bea7f2e2f66
Jing En <wongje11@gmail.com>
2023-03-05 15:47:14 +0800

    Change location of styles.css

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,5 @@ export default {
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap");
-@import url("../../styles.css");
+@import url("../styles.css");
 </style>
