a1b8f01dd879c534d3337373a2300b78f619a7fc
whyyouj <yujie251100@gmail.com>
2023-03-13 20:16:35 +0800

    Add [FE] Added UniversityTiles to ListofUniversities Page

--- src/components/ListOfUniversities.vue
+++ src/components/ListOfUniversities.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    List of Universites page, should contain all unis split by continent
+    <h2>List of Exchange Universities</h2>
+    <div class="filter-bar">
+      <ul>
+        <li v-for="continent in continents"
+            :key="continent.id"
+            :class="{ active: continent.active }"
+            @click="toggleContinent(continent)">
+          {{ continent.name }}
+        </li>
+      </ul>
+    </div>
+    <div class="tile-list">
+      <UniTile v-for="university in filteredUniversities"
+               :key="university.id"
+               :name="university.name"
+               :country="university.country"
+               :continent="university.continent"
+               :gpa="university.gpa"
+               :imageUrl="university.imageUrl"
+               :universityUrl="university.url"
+               class="tile" />
+    </div>
   </div>
 </template>
 
