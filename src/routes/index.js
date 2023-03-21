3f5973c42d8c06a0a751232cf6a0cc283c18fc7e
morksen123 <morksen123@gmail.com>
2023-03-22 00:46:22 +0800

    added routes folder to store all external links

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+// general help page
+export const NUS_FINANCIAL_AID_URL = "https://www.nus.edu.sg/oam/financial-aid/loans/overseas-student-exchange-programme-(sep)-loan";
+export const NUS_STUDENT_TRAVEL_INSURANCE_URL = "https://myportal.nus.edu.sg/studentportal/student-insurance/all/";
+export const THIRD_PARTY_TRAVEL_INSURANCE_URL = "https://www.income.com.sg/travel-insurance";
+export const INTERNATIONAL_SOS_URL = "https://www.internationalsos.com/locations/singapore";
