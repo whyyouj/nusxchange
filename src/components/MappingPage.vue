ee2e25febe6cf4d04d9bbf962d85223456038c00
whyyouj <e0725452@u.nus.edu>
2023-03-23 15:12:04 +0800

    Add [FE] Search Bar

--- src/components/MappingPage.vue
+++ src/components/MappingPage.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="main">
-    <h2> Module Mapping Page </h2>
+    <h2>Module Mapping Page</h2>
     <img src="https://wallpapercave.com/wp/wp1954746.jpg" alt="" style="width: 100%; border-radius: 15px;">
+    <div class="search-container">
+      <input type="text" placeholder="Search...">
+      <button class="add-module-btn">Add Module</button>
+    </div>
   </div>
 </template>
-
-<script>
-import firebaseApp from '../firebase.js';
-import {getFirestore} from 'firebase/firestore'
-import {collection, getDocs} from 'firebase/firestore';
-
-const db = getFirestore(firebaseApp);
-db,collection,getDocs
-export default {
-
-};
-</script>
 
 <style scoped>
 .main {
   margin: auto;
   max-width: 80%;
+}
+
+.search-container {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+}
+
+input[type=text] {
+  flex: 1;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  font-size: 16px;
+  margin-right: 10px;
+}
+
+.add-module-btn {
+  background-color: #4CAF50;
+  color: white;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 12px 20px;
+}
+
+.add-module-btn:hover {
+  background-color: #45a049;
 }
 </style>
