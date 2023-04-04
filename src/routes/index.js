4cb770dc6579c4574a9bb33d04d19dd84c881145
morksen123 <morksen123@gmail.com>
2023-04-04 10:47:32 +0800

    updated external links for home page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,3 +3,13 @@ export const NUS_FINANCIAL_AID_URL = "https://www.nus.edu.sg/oam/financial-aid/l
 export const NUS_STUDENT_TRAVEL_INSURANCE_URL = "https://myportal.nus.edu.sg/studentportal/student-insurance/all/";
 export const THIRD_PARTY_TRAVEL_INSURANCE_URL = "https://www.income.com.sg/travel-insurance";
 export const INTERNATIONAL_SOS_URL = "https://www.internationalsos.com/locations/singapore";
+
+// home page
+export const KYOTO_UNIVERSITY_IMAGE = "https://smapse.com/storage/2019/10/a3-3.jpg";
+export const ARIZONA_STATE_IMAGE = "https://campus.asu.edu/sites/default/files/2021-08/Hayden%20library.jpeg";
+export const TOKYO_UNIVERSITY_IMAGE = "https://s3-ap-northeast-1.amazonaws.com/thegate/2021/01/14/11/51/07/Tokyo-University.jpg";
+export const TRINITY_DUBLIN_COLLEGE_IMAGE = "https://img.theculturetrip.com/wp-content/uploads/2016/08/6995209237_5244628609_k.jpg";
+export const ETH_ZURICH_IMAGE = "https://ethz.ch/en/news-and-events/eth-news/news/2023/03/14-professors-appointed/_jcr_content/pageimages/imageCarousel.imageformat.carousel.1519103602.jpg";
+export const UNIVERSITY_WESTERN_AUSTRALIA = "https://www.studyabroad.sg/site/wp-content/uploads/UWA.jpg";
+export const EINDHOVEN_IMAGE = "https://images.adsttc.com/media/images/57ed/e74c/e58e/ce3d/8200/00af/large_jpg/01_Team_RSVP_TUE_CamPlein.jpg?1475209012";
+export const KOREA_UNIVERSITY_IMAGE = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/ed/6a/ee/inchon-hall.jpg?w=1200&h=-1&s=1";
